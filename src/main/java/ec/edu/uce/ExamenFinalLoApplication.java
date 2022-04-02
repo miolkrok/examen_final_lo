1a614b13857f2cb0a4ee1010a9e245abb9643144
Luis Ortiz <luchoaam97@gmail.com>
2022-04-01 20:01:04 -0500

    primer commit 2.0

---
+++ src/main/java/ec/edu/uce/ExamenFinalLoApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.uce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenFinalLoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenFinalLoApplication.class, args);
+	}
+
+}
